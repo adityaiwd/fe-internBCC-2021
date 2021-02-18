427b9e69a6cf28d5aac01d5e10f2755f759650e9
Wicak <adityad.wck@gmail.com>
2021-02-18 10:55:18 +0700

    finished starting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SplashScreen from "./components/SplashScreen";
+import GlobalStyle from "./theme/globalStyles"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:"100%",height:"100vh",display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <GlobalStyle />
+      <SplashScreen />
     </div>
   );
 }
---
+++ src/components/CreateArea.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { TextField, Button } from "@material-ui/core";
+
+export const TweetArea = styled.div`
+  background-color: #fff;
+  margin: 0 auto;
+  margin-top: 10rem;
+  min-height: 5rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 1rem;
+  padding: 2rem;
+  box-shadow:0 2rem 6rem rgba(0,0,0,.3);
+`;
+
+export const InputTweet = styled(TextField).attrs(() => ({
+    InputProps: { style: { fontSize: "1.6rem", fontFamily: "inherit" } },
+    InputLabelProps:{style:{ fontSize: "1.6rem", fontFamily: "inherit" }},
+    variant:"outlined" 
+}))`
+    width:100%;
+`
+
+export const TweetButton = styled(Button)`
+    align-self:flex-end;
+    font-size:1.3rem;
+    
+`
